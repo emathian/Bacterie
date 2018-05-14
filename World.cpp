7dd729691a56e873b884782eef0b3d372a9fcebc
Marine Djaffardjy <marine.djaffardjy@insa-lyon.fr>
2018-05-14 18:05:01 +0200

    competition a finir

--- World.cpp
+++ World.cpp
@@ -4,6 +4,7 @@
 #include "Bacteria.h"
 #include "BacteriaGa.h"
 #include "World.h"
+#include <map>
 
 // ===========================================================================
 //                       Definition of static attributes
@@ -61,10 +62,8 @@ void diffuse_concentration(){
   int stockC = c_;
   int x;
   int y;
-  int i;
-  int j;
-    for(x = 0; i<W_; i++){
-      for(y = 0; j<H_; j++){
+    for(x = 0; x<W_; x++){ //differents cas pour la forme toroïdale
+      for(y = 0; y<H_; y++){
         if(x = 0){
           xg = W_ - 1;
           xd = x + 1;
@@ -108,11 +107,50 @@ void diffuse_concentration(){
 
 
   void competition(){
-    
+    map<Bacteria *,float> neighborhood;
     int x;
     int y;
-    int i;
-    int j;
+    for(x = 0; x< W_; x++){
+      for(y = 0; y< H_; y++){
+        if(pop_[x][y] = NULL){
+          if(x = 0){
+            xg = W_ - 1;
+            xd = x + 1;
+          }else{
+            if(x = W_-1){
+              xg = x - 1;
+              xd = 0;
+            }else{
+              xg = x - 1;
+              xd = x + 1;
+            }
+         }
+          if(y = 0){
+            yh = H_ - 1;
+           yb = y + 1;        
+          }else{
+            if(y = H_-1){
+              yh = y - 1;
+              yb = 0; 
+            }else{
+             yh = y - 1;
+              yb = y + 1;          
+            }
+          }
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xg][yh],pop_[xg][yh].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xg][y],pop_[xg][y].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xg][yb],pop_[xg][yb].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xd][yh],pop_[xd][yh].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xd][y],pop_[xd][y].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[xd][yb],pop_[xd][yb].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[x][yh],pop_[x][yh].get_fitness()) );
+        neighborhood.insert ( std::pair<Bacteria *,float>(pop_[x][yb],pop_[x][yb].get_fitness()) );
+        std::map<Bacteria *,float>::iterator it = neighborhood.begin();
+        // faire divide ici
+        }      
+      }
+    }
+      
     
 }
   
