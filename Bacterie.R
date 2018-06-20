6fe7141c55915b5d29682a8964aa7bc5cf1f2937
Lisa Nicvert <lisa.nicvert@insa-lyon.fr>
2018-06-20 07:13:40 +0200

    Modified R plot

--- Bacterie.R
+++ Bacterie.R
@@ -4,8 +4,6 @@
 
 rm(list=ls())
 
-library(colorspace)
-
 #----------------------
 # p0
 # ---------------------
@@ -21,16 +19,19 @@ names(p0)[names(p0)=="V2"] <- "popA"
 names(p0)[names(p0)=="V3"] <- "Trenew"
 names(p0)[names(p0)=="V4"] <- "A"
 
-color <- cut(p0$popA, breaks=c(0,0.2,0.5,0.8,1), labels=sequential_hcl(4))
+extinction = (p0$popB == 0 & p0$popA == 0)
+exclusion = ((p0$popB== 0 & p0$popA != 0) | (p0$popA==0 & p0$popB != 0))
+cohabitation = (p0$popB != 0 & p0$popA != 0)
 
-plot(p0$A~p0$Trenew,pch=15,col=color) 
+plot(p0$A[exclusion]~p0$Trenew[exclusion],pch=15,col='orange')
+points(p0$A[extinction]~p0$Trenew[extinction],pch=15,col='red')
+points(p0$A[cohabitation]~p0$Trenew[cohabitation],pch=15,col='green')
 
 #----------------------
 # p0.0001
 # ---------------------
 rm(list=ls())
 
-library(colorspace)
 p00001 <- read.table("/Users/lisa/Documents/Cours/3-BiM/Projet_DevLog/Bacterie/csv/pM_0001_A017_DT50",sep=";",dec=".")
 p000012 <- read.table("/Users/lisa/Documents/Cours/3-BiM/Projet_DevLog/Bacterie/csv/a_18_33_pmute00001.csv",sep=";",dec=".")
 
@@ -43,6 +44,10 @@ names(p00001)[names(p00001)=="V2"] <- "popA"
 names(p00001)[names(p00001)=="V3"] <- "Trenew"
 names(p00001)[names(p00001)=="V4"] <- "A"
 
-color <- cut(p00001$popA, breaks=c(0,0.2,0.5,0.8,1), labels=sequential_hcl(4))
+extinction = (p00001$popB == 0 & p00001$popA == 0)
+exclusion = ((p00001$popB== 0 & p00001$popA != 0) | (p00001$popA==0 & p00001$popB != 0))
+cohabitation = (p00001$popB != 0 & p00001$popA != 0)
 
-plot(p00001$A~p00001$Trenew,pch=15,col=color) 
+plot(p00001$A[exclusion]~p00001$Trenew[exclusion],pch=15,col='orange')
+points(p00001$A[extinction]~p00001$Trenew[extinction],pch=15,col='red')
+points(p00001$A[cohabitation]~p00001$Trenew[cohabitation],pch=15,col='green')
